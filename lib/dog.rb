9fbf32e652920119dd408f4962f66d70055c606f
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 15:34:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
+  def name=(dogs_name)
+  @name = dogs_name
 end
 
-def name
-  "#{@name}".strip
-  end
-end
-
-initialize(name)
+7.	  def name
+8.	    @this_dogs_name
+9.	  end
+10.	end
+11.	 
+12.	lassie = Dog.new
+13.	lassie.name = "Lassie"
+14.	 
+15.	puts lassie.name
+16.	 
