24955e2ef07719620b9698b90eb983aee9418c80
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 15:16:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,6 +12,21 @@ fido = Dog.new
   fido.name = "Fido"
 puts fido.name
 
+def name=(dogs_name)
+4.	    @this_dogs_name = dogs_name
+5.	  end
+6.	 
+7.	  def name
+8.	    @this_dogs_name
+9.	  end
+10.	end
+11.	 
+12.	lassie = Dog.new
+13.	lassie.name = "Lassie"
+14.	 
+15.	puts lassie.name
+16.	 
+
 class Breed
 def name=(dogs_breed)
   @breed = dogs_breed
