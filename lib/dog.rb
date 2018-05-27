5fe9d38c6110cc45b6ca40fe3d3e90adad3cc6e5
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 15:07:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,9 @@
 class Dog
-  
+def name=(dogs_name)
+  @this_dogs_name = dogs_name
 end
 
-1.	class Dog
-2.	  def name=(dog_name)
-3.	    this_dogs_name = dog_name
-4.	  end
-5.	 
-6.	  def name
-7.	    this_dogs_name
-8.	  end
-9.	end
+def name
+  @this_dogs_name
+  end
+end
